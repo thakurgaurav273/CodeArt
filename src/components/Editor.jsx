9819053e67fcce505a767daff567ba5c1642d60c
gaurav singh <thakurgaurav273@gmail.com>
2023-09-13 20:57:42 +0530

    first commit of some common file

---
+++ src/components/Editor.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "codemirror/lib/codemirror.css"
+import "codemirror/theme/material.css"
+import "codemirror/mode/xml/xml"
+import "codemirror/mode/javascript/javascript"
+import "codemirror/mode/css/css"
+import { Controlled as ControlledEditor } from "react-codemirror2";
+export default function Editor() {
+  return (
+    <div className="editor-container">
+      
+    </div>
+  )
+}
+
